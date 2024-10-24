67c76471ebe9f0663bc61a58a44d9e24470346ba
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2024-10-23 22:40:00 -0300

    Make phone number validation regex of Reservation model simpler

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,7 +1,7 @@
 class Reservation < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :phone_number, presence: true, format: { with: /\A\+?[0-9]{10,15}\z/, message: "must be a valid phone number" }
+  validates :phone_number, presence: true, format: { with: /\d{10,11}\z/, message: "must be a valid phone number" }
   validates :number_of_guests, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 9, message: "must be between 1 and 9 (9+)" }
   validates :date, presence: true
   validates :time, presence: true, format: { with: /\A([01]\d|2[0-3]):([0-5]\d)\z/, message: "must be in the format HH:MM" }
