25f81874ef7f5062d6c854108770ced0c009ebd3
ZaikoXander <80606136+ZaikoXander@users.noreply.github.com>
2024-10-29 21:36:16 -0300

    Uncomment reservations routes requests

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,17 +1,17 @@
 class ReservationsController < ApplicationController
-  # before_action :set_reservation, only: %i[ show update destroy ]
+  before_action :set_reservation, only: %i[ show update destroy ]
 
   # GET /reservations
-  # def index
-  #   @reservations = Reservation.all
+  def index
+    @reservations = Reservation.all
 
-  #   render json: @reservations
-  # end
+    render json: @reservations
+  end
 
   # GET /reservations/1
-  # def show
-  #   render json: @reservation
-  # end
+  def show
+    render json: @reservation
+  end
 
   # POST /reservations
   def create
@@ -25,24 +25,24 @@ class ReservationsController < ApplicationController
   end
 
   # PATCH/PUT /reservations/1
-  # def update
-  #   if @reservation.update(reservation_params)
-  #     render json: @reservation
-  #   else
-  #     render json: @reservation.errors, status: :unprocessable_entity
-  #   end
-  # end
+  def update
+    if @reservation.update(reservation_params)
+      render json: @reservation
+    else
+      render json: @reservation.errors, status: :unprocessable_entity
+    end
+  end
 
   # DELETE /reservations/1
-  # def destroy
-  #   @reservation.destroy!
-  # end
+  def destroy
+    @reservation.destroy!
+  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    # def set_reservation
-    #   @reservation = Reservation.find(params[:id])
-    # end
+    def set_reservation
+      @reservation = Reservation.find(params[:id])
+    end
 
     # Only allow a list of trusted parameters through.
     def reservation_params
